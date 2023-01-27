d3f3996840238379d5e32b803a03046b7554a563
LanselleD <72977738+LanselleD@users.noreply.github.com>
2023-01-27 16:39:07 +0100

    Ajout class Personne, admin, client + mapping

--- Projet_Final_Bar_a_Jeux_back/src/main/java/model/Client.java
+++ Projet_Final_Bar_a_Jeux_back/src/main/java/model/Client.java
@@ -1,12 +1,92 @@
 package model;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Transient;
+
+@Entity
+@DiscriminatorValue("Client")
 public class Client extends Compte {
 
+	@Column(length = 100)
+	private String nom;
+	@Column(length = 100)
+	private String prenom;
+	@Column(length = 50)
+	private String tel;
+	private LocalDate dateNaissance;
+	@Transient
+	private List<Reservation> reservations = new ArrayList<>();
+	@Transient
+	private List<CommandeJeu> commandeJeux = new ArrayList<>();
+	@Enumerated(EnumType.STRING)
+	private Civilite civilite;
 	
-	public transient Map<Jeux,Integer> panier = new HashMap<Jeux, Integer>();
 	
-	
+	public Client() {
+		super();
+	}
+		
+	public String getNom() {
+		return nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public LocalDate getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public List<Reservation> getReservations() {
+		return reservations;
+	}
+
+	public List<CommandeJeux> getCommandesJeux() {
+		return commandesJeux;
+	}
+
+	public Civilite getCivilite() {
+		return civilite;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public void setDateNaissance(LocalDate dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	public void setReservations(List<Reservation> reservations) {
+		this.reservations = reservations;
+	}
+
+	public void setCommandesJeux(List<CommandeJeux> commandesJeux) {
+		this.commandesJeux = commandesJeux;
+	}
+
+	public void setCivilite(Civilite civilite) {
+		this.civilite = civilite;
+	}
 }
--- Projet_Final_Bar_a_Jeux_back/src/main/java/model/Compte.java
+++ Projet_Final_Bar_a_Jeux_back/src/main/java/model/Compte.java
@@ -1,5 +1,54 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type_personne", discriminatorType = DiscriminatorType.STRING)
 public abstract class Compte {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(length = 100)
+	private String mail;
+	@Column(length = 100)
+	private String password;
+	
+	public Compte() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
