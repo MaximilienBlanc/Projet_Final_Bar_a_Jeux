7c4c893919c1f6cebf56368e0d28e74011f9c3ac
LanselleD <72977738+LanselleD@users.noreply.github.com>
2023-02-09 08:35:08 +0100

    modif import

--- Projet_Final_Bar_a_Jeux_front/src/main/java/controller/ClientController.java
+++ Projet_Final_Bar_a_Jeux_front/src/main/java/controller/ClientController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import service.ClientService;
+import quest.service.ClientService;
 
 @Controller
 @RequestMapping("/clientInfo")
